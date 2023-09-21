1eac11de9ae72be9142266075046fa9daeb4ee10
Penttala Janne O <janne_96_@hotmail.com>
2023-09-21 17:42:36 +0300

    emission reduction calculations for data

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_csv("emissions_CO2_2011-2021.csv", header=1)
-print(data.head())
-change_data = data[2021] / data[2011] -1
+data.rename(columns={"Unnamed: 0": "Year"}, inplace=True)
+data = data.set_index('Year')
+data = data.apply(pd.to_numeric, errors='coerce')
+
+selected_data = data.loc[["2011", "2021"]]
+emission_reductions = ((selected_data.loc["2021"] - selected_data.loc["2011"]) / selected_data.loc["2011"]) * 100
+
+print(emission_reductions)
