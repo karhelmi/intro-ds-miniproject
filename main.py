2f7b5370571c33b2ff3ae44599dc88c432aa32f3
Penttala Janne O <janne_96_@hotmail.com>
2023-10-03 14:34:06 +0300

    finland dataframe created

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 import numpy as np
 import pandas as pd
 
-#In MtCO2
-data_CO2 = pd.read_csv("emissions_CO2_2011-2021.csv", header=1) #source: https://globalcarbonatlas.org/
+# In MtCO2
+# Source: https://globalcarbonatlas.org/
+data_CO2 = pd.read_csv("emissions_CO2_2011-2021.csv", header=1)
 data_CO2.rename(columns={"Unnamed: 0": "Year"}, inplace=True)
 data_CO2 = data_CO2.set_index('Year')
-data_CO2 = data_CO2.drop([np.NaN,'SOURCES','Territorial'])
+data_CO2 = data_CO2.drop([np.NaN, 'SOURCES', 'Territorial'])
 data_CO2 = data_CO2.apply(pd.to_numeric, errors='coerce')
 emission_reductions_CO2 = ((data_CO2.loc["2021"] - data_CO2.loc["2011"]) / data_CO2.loc["2011"]) * 100
-#print("Sorted based on MtCO2")
-#print(emission_reductions_CO2.sort_values().to_string())
-print(data_CO2)
+print("Sorted based on MtCO2")
+print(emission_reductions_CO2.sort_values().to_string())
 
-#In kgCO2/GDP
-data_CO2_GDP = pd.read_csv("emissions_CO2_GDP_2011-2021.csv", header=1) #source: https://globalcarbonatlas.org/
+# In kgCO2/GDP
+# Source: https://globalcarbonatlas.org/
+data_CO2_GDP = pd.read_csv("emissions_CO2_GDP_2011-2021.csv", header=1)
 data_CO2_GDP.rename(columns={"Unnamed: 0": "Year"}, inplace=True)
 data_CO2_GDP = data_CO2_GDP.set_index('Year')
 data_CO2_GDP = data_CO2_GDP.apply(pd.to_numeric, errors='coerce')
 emission_reductions_CO2_GDP = ((data_CO2_GDP.loc["2021"] - data_CO2_GDP.loc["2011"]) / data_CO2_GDP.loc["2011"]) * 100
-#print("\nSorted based on kgCO2/GDP")
-#print(emission_reductions_CO2_GDP.sort_values().to_string())
+print("\nSorted based on kgCO2/GDP")
+print(emission_reductions_CO2_GDP.sort_values().to_string())
 
-#In tCO2/person
-data_CO2_person = pd.read_csv("emissions_CO2_person_2011-2021.csv", header=1) #source: https://globalcarbonatlas.org/
+# In tCO2/person
+# Source: https://globalcarbonatlas.org/
+data_CO2_person = pd.read_csv("emissions_CO2_person_2011-2021.csv", header=1)
 data_CO2_person.rename(columns={"Unnamed: 0": "Year"}, inplace=True)
 data_CO2_person = data_CO2_person.set_index('Year')
 data_CO2_person = data_CO2_person.apply(pd.to_numeric, errors='coerce')
 emission_reductions_CO2_person = ((data_CO2_person.loc["2021"] - data_CO2_person.loc["2011"]) / data_CO2_person.loc["2011"]) * 100
-#print("\nSorted based on tCO2/person")
-#print(emission_reductions_CO2_person.sort_values().to_string())
+print("\nSorted based on tCO2/person")
+print(emission_reductions_CO2_person.sort_values().to_string())
 
-data_electricity_renewables = pd.read_csv("share-electricity-renewables.csv")
-data_electricity_renewables.drop('Code', axis=1, inplace=True)
-data_electricity_renewables = data_electricity_renewables[(data_electricity_renewables['Year'] >= 2011) & (data_electricity_renewables['Year'] <= 2021)]
-#print(data_electricity_renewables)
-
+# Function to get data of specific country
 def filter_by_country(df, country):
     filtered_df = df[df['Entity'] == country]
     return filtered_df
 
-filtered_finland_df = filter_by_country(data_electricity_renewables, 'Finland')
-filtered_finland_df = filtered_finland_df.set_index('Year')
-#print(filtered_finland_df)
+# Original dataframe for Finland; new columns can be added to this
+finland_df = data_CO2['Finland'].to_frame()
+finland_df.rename(columns={'Finland': 'CO2_emissions'}, inplace=True)
+finland_df.index = finland_df.index.astype(int)
 
-filtered_finland_df['CO2_emissions'] = data_CO2.loc['2011':'2021']['Finland'].index.astype('int64')
-#merged_df = filtered_finland_df.merge(data_CO2['Finland'], left_index=True, right_index=True, how='inner')
-#print(data_CO2['Finland'].index.dtype)
-#print(merged_df)
-print(filtered_finland_df)
+# Source: https://ourworldindata.org/
+electricity_renewables_df = pd.read_csv("share-electricity-renewables.csv")
+electricity_renewables_df = electricity_renewables_df[(electricity_renewables_df['Year'] >= 2011) & (electricity_renewables_df['Year'] <= 2021)]
+
+# Filter data from finland and merge it into original dataframe
+finland_electricity_renewables_df = filter_by_country(electricity_renewables_df, 'Finland')
+finland_electricity_renewables_df = finland_electricity_renewables_df.set_index('Year')
+finland_electricity_renewables_df = finland_electricity_renewables_df['Renewables (% electricity)'].to_frame()
+finland_electricity_renewables_df.index = finland_electricity_renewables_df.index.astype(int)
+finland_df = finland_df.join(finland_electricity_renewables_df)
+
+print("\nFinland dataframe")
+print(finland_df)
