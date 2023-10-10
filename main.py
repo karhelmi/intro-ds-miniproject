9b0e462c6fc3364f76232e287963493d8c9eb5a1
karhelmi <helmi.karesti@helsinki.fi>
2023-10-10 22:06:15 +0300

    changed few csv files. also, added an r-squared table to main.py

--- main.py
+++ main.py
@@ -37,39 +37,29 @@ print(emission_reductions_CO2_person.sort_values().to_string())
 # Dictionary of the csv files; filename as key and wanted column as value
 csv_column_dict = {
     'csv_data/1_global-meat-production.csv': 'Meat, total | 00001765 || Production | 005510 || tonnes',
-    'csv_data/2_meat-supply-per-person.csv': 'Meat, total | 00002943 || Food available for consumption | 0645pc || kilograms per year per capita', #Data missing from 2021
+    'csv_data/2_life-expectancy.csv': 'Life expectancy at birth (historical)', #NEW
     'csv_data/3_gdp-per-capita-worldbank.csv': 'GDP per capita, PPP (constant 2017 international $)',
-    'csv_data/4_historical-gov-spending-gdp.csv': 'Government Expenditure (IMF based on Mauro et al. (2015))', #Data missing from 2012-2021
-    'csv_data/5_share-of-individuals-using-the-internet.csv': 'Individuals using the Internet (% of population)',
-    'csv_data/6_research-spending-gdp.csv': 'Research and development expenditure (% of GDP)', #Data missing from 2021
-    'csv_data/7_human-development-index.csv': 'Human Development Index',
-    'csv_data/8_human-rights-index-vdem.csv': 'civ_libs_vdem_owid', # Unsure about the column
-    'csv_data/9_self-reported-trust-attitudes.csv': 'Agree "Most people can be trusted"', #No data from Finland
-    'csv_data/10_co2-emissions-transport.csv': 'Transport', #Data missing from 2020-2021
-    'csv_data/11_global-fossil-fuel-consumption.csv': 'Gas (TWh, direct energy)', # No data from specific countries
-    'csv_data/12_nuclear-energy-generation.csv': 'Electricity from nuclear (TWh)', 
-    'csv_data/13_per-capita-energy-use.csv': 'Primary energy consumption per capita (kWh/person)',
-    'csv_data/14_annual-freshwater-withdrawals.csv': 'Annual freshwater withdrawals, total (billion cubic meters)', #No data from 2020-2021
-    'csv_data/15_share-electricity-renewables.csv': 'Renewables (% electricity)'
+    'csv_data/4_share-of-individuals-using-the-internet.csv': 'Individuals using the Internet (% of population)',
+    'csv_data/5_human-development-index.csv': 'Human Development Index',
+    'csv_data/6_human-rights-index-vdem.csv': 'civ_libs_vdem_owid', # Central estimate chosen 10.10.23
+    'csv_data/7_population-and-demography.csv': 'Population', #NEW
+    'csv_data/8_nuclear-energy-generation.csv': 'Electricity from nuclear (TWh)', 
+    'csv_data/9_per-capita-energy-use.csv': 'Primary energy consumption per capita (kWh/person)',
+    'csv_data/10_share-electricity-renewables.csv': 'Renewables (% electricity)'
 }
 
 # Variables we want to include to a dataframe of a country; Uncomment to include in the dataframe
 variable_csv_list = [
-     'csv_data/1_global-meat-production.csv',
-#    'csv_data/2_meat-supply-per-person.csv',
-     'csv_data/3_gdp-per-capita-worldbank.csv',
-#    'csv_data/4_historical-gov-spending-gdp.csv',
-     'csv_data/5_share-of-individuals-using-the-internet.csv',
-#    'csv_data/6_research-spending-gdp.csv',
-     'csv_data/7_human-development-index.csv',
-#    'csv_data/8_human-rights-index-vdem.csv',
-#    'csv_data/9_self-reported-trust-attitudes.csv',
-#    'csv_data/10_co2-emissions-transport.csv',
-#    'csv_data/11_global-fossil-fuel-consumption.csv',
-     'csv_data/12_nuclear-energy-generation.csv',
-     'csv_data/13_per-capita-energy-use.csv',
-#    'csv_data/14_annual-freshwater-withdrawals.csv',
-    'csv_data/15_share-electricity-renewables.csv'
+    'csv_data/1_global-meat-production.csv',
+    'csv_data/2_life-expectancy.csv',
+    'csv_data/3_gdp-per-capita-worldbank.csv',
+    'csv_data/4_share-of-individuals-using-the-internet.csv',
+    'csv_data/5_human-development-index.csv',
+    'csv_data/6_human-rights-index-vdem.csv',
+    'csv_data/7_population-and-demography.csv',
+    'csv_data/8_nuclear-energy-generation.csv',
+    'csv_data/9_per-capita-energy-use.csv',
+    'csv_data/10_share-electricity-renewables.csv'
 ]
 
 # Create original dataframe for a country with CO2
@@ -98,11 +88,16 @@ def filter_data(variable_df, country_df, country, column):
     country_df.name = country #This should add a name to the dataframe. Needed in plotting.
     return country_df
 
+# Create an empty dataframe with an index for r-squared values of the different countries
+r_df_country = pd.DataFrame(index=range(1,11)) ## NEW
+
 # Draw a scatter plot with linear regression line & calculate R-squared for the model
 def plot_country_variables_vs_CO2(country_df):  
     num_columns = 2
     num_rows = country_df.shape[1] // 2
     plt.figure(figsize=(15, 10))
+
+    r_list_country = [] # Create a list to add the different R-squared figures of a country.
 
     for column_index in range(1,country_df.shape[1]):
         selected_column = country_df.iloc[:,column_index]
@@ -119,10 +114,12 @@ def plot_country_variables_vs_CO2(country_df):
         model.fit(country_df.iloc[:,column_index].values.reshape(-1,1), country_df.iloc[:,0].values.reshape(-1,1))
         y_fitted = model.predict(country_df.iloc[:,column_index].values.reshape(-1,1))
         r_squared = model.score(country_df.iloc[:,column_index].values.reshape(-1,1), country_df.iloc[:,0].values.reshape(-1,1))
-        
+        r_list_country.append("{:.2f}".format(r_squared))  # Add the r-squared value of the variable to the list
+
         plt.text(0.8,1.1,f"R squared: {r_squared:.2f}", fontsize=12, color="green", transform=plt.gca().transAxes)
         plt.plot(selected_column, y_fitted, color="green")
 
+    r_df_country[f"{country_df.name}"] = r_list_country # Add the r-squared values of the country to the R-squared dataframe
     plt.tight_layout()
     plt.show()
 
@@ -141,8 +138,13 @@ def country_df_with_data(country, variable_csv_list):
 
 finland_df = country_df_with_data('Finland', variable_csv_list)
 estonia_df = country_df_with_data('Estonia', variable_csv_list)
+sweden_df = country_df_with_data('Sweden', variable_csv_list)
 
 print("\nFinland dataframe")
 print(finland_df)
 print("\nEstonia dataframe")
 print(estonia_df)
+print("\nSweden dataframe")
+print(sweden_df)
+print("\nR-squared table of the different countries for the variables 1-10")
+print(r_df_country)
